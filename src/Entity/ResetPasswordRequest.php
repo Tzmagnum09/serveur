0191c487543846bc9fa807bd33195cbcd45b8493
root <root@dmqode.be>
2025-04-03 17:02:06 +0200

    update 03-04-2025

--- src/Entity/ResetPasswordRequest.php
+++ src/Entity/ResetPasswordRequest.php
@@ -3,78 +3,37 @@
 namespace App\Entity;
 
 use App\Repository\ResetPasswordRequestRepository;
-use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use SymfonyCasts\Bundle\ResetPassword\Model\ResetPasswordRequestInterface;
+use SymfonyCasts\Bundle\ResetPassword\Model\ResetPasswordRequestTrait;
 
 #[ORM\Entity(repositoryClass: ResetPasswordRequestRepository::class)]
-class ResetPasswordRequest
+class ResetPasswordRequest implements ResetPasswordRequestInterface
 {
+    use ResetPasswordRequestTrait;
+
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\Column(length: 255)]
-    private ?string $selector = null;
-
-    #[ORM\Column(length: 255)]
-    private ?string $hashedToken = null;
-
-    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
-    private ?\DateTimeInterface $requestedAt = null;
-
-    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
-    private ?\DateTimeInterface $expiresAt = null;
+    #[ORM\ManyToOne]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?User $user = null;
 
     public function getId(): ?int
     {
         return $this->id;
     }
 
-    public function getSelector(): ?string
+    public function getUser(): object
     {
-        return $this->selector;
+        return $this->user;
     }
 
-    public function setSelector(string $selector): static
+    public function setUser(object $user): self
     {
-        $this->selector = $selector;
-
-        return $this;
-    }
-
-    public function getHashedToken(): ?string
-    {
-        return $this->hashedToken;
-    }
-
-    public function setHashedToken(string $hashedToken): static
-    {
-        $this->hashedToken = $hashedToken;
-
-        return $this;
-    }
-
-    public function getRequestedAt(): ?\DateTimeInterface
-    {
-        return $this->requestedAt;
-    }
-
-    public function setRequestedAt(\DateTimeInterface $requestedAt): static
-    {
-        $this->requestedAt = $requestedAt;
-
-        return $this;
-    }
-
-    public function getExpiresAt(): ?\DateTimeInterface
-    {
-        return $this->expiresAt;
-    }
-
-    public function setExpiresAt(\DateTimeInterface $expiresAt): static
-    {
-        $this->expiresAt = $expiresAt;
+        $this->user = $user;
 
         return $this;
     }
