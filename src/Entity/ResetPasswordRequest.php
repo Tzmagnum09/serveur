dc96725cfaf06ada215165676fb2595c8a287c6b
root <tzmagnum@gmail.com>
2025-04-05 02:26:19 +0200

    update 05-04-2025 02h26

--- src/Entity/ResetPasswordRequest.php
+++ src/Entity/ResetPasswordRequest.php
@@ -8,7 +8,6 @@ use SymfonyCasts\Bundle\ResetPassword\Model\ResetPasswordRequestInterface;
 use SymfonyCasts\Bundle\ResetPassword\Model\ResetPasswordRequestTrait;
 
 #[ORM\Entity(repositoryClass: ResetPasswordRequestRepository::class)]
-#[ORM\Table(name: 'reset_password_request')]
 class ResetPasswordRequest implements ResetPasswordRequestInterface
 {
     use ResetPasswordRequestTrait;
@@ -18,82 +17,24 @@ class ResetPasswordRequest implements ResetPasswordRequestInterface
     #[ORM\Column(type: 'integer')]
     private ?int $id = null;
 
-    #[ORM\ManyToOne(targetEntity: User::class)]
-    #[ORM\JoinColumn(nullable: false, name: 'user_id', referencedColumnName: 'id', onDelete: 'CASCADE')]
+    #[ORM\ManyToOne]
+    #[ORM\JoinColumn(nullable: false, onDelete: "CASCADE")]
     private ?User $user = null;
-
-    #[ORM\Column(type: 'datetime')]
-    private \DateTimeInterface $expiresAt;
-
-    #[ORM\Column(type: 'string', length: 100)]
-    private string $selector;
-
-    #[ORM\Column(type: 'string', length: 100)]
-    private string $hashedToken;
-
-    #[ORM\Column(type: 'datetime')]
-    private \DateTimeInterface $createdAt;
-
-    public function __construct()
-    {
-        $this->createdAt = new \DateTimeImmutable();
-    }
 
     public function getId(): ?int
     {
         return $this->id;
     }
 
-    public function getUser(): User
+    public function getUser(): object
     {
         return $this->user;
     }
 
-    public function setUser(User $user): self
+    public function setUser(object $user): self
     {
         $this->user = $user;
 
         return $this;
-    }
-
-    public function getExpiresAt(): \DateTimeInterface
-    {
-        return $this->expiresAt;
-    }
-
-    public function setExpiresAt(\DateTimeInterface $expiresAt): self
-    {
-        $this->expiresAt = $expiresAt;
-
-        return $this;
-    }
-
-    public function getSelector(): string
-    {
-        return $this->selector;
-    }
-
-    public function setSelector(string $selector): self
-    {
-        $this->selector = $selector;
-
-        return $this;
-    }
-
-    public function getHashedToken(): string
-    {
-        return $this->hashedToken;
-    }
-
-    public function setHashedToken(string $hashedToken): self
-    {
-        $this->hashedToken = $hashedToken;
-
-        return $this;
-    }
-
-    public function getCreatedAt(): \DateTimeInterface
-    {
-        return $this->createdAt;
     }
 }
